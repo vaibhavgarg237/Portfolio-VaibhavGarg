a79f46938fbd94969ed7046514199626d617a24c
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-11-08 22:42:04 +0530

    feat: react-router-dom setup, added major components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/work" element={<Work />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
